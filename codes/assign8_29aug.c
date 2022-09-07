849197fbf6e0b9f04e15e3376421b89fde8de34f
rk-1620 <109271418+rk-1620@users.noreply.github.com>
2022-09-07 18:22:49 +0530

    Add files via upload

---
+++ codes/assign8_29aug.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main(){
+printf("enter the data in 2D array\n");
+int r,c;
+printf("enter the row of matrix = ");
+scanf("%d",&r);
+printf("enter the column of matrix = ");
+scanf("%d",&c);
+int arr[10][10];
+int l,m;
+int nz;
+printf("enter the total number of non zero elements =  ");
+scanf("%d",&nz);
+int i,j,k,e;
+for(l=0;l<r;l++){
+    for(m=0;m<c;m++){
+    	arr[l][m]=0;
+	}
+}
+for(k=1;k<=nz;k++){
+	printf("enter the elments %d with row,column,and value respectively = ",k );
+	scanf("%d %d %d",&i,&j,&e);
+	arr[i][j]=e;
+}
+printf("\n matrix \n");
+for(l=0;l<r;l++){
+    for(m=0;m<c;m++){
+		printf("%d\t",arr[l][m]);
+	}
+    printf("\n");
+}
+printf("\n transpose \n");
+for(l=0;l<c;l++){
+    for(m=0;m<r;m++){
+       printf("%d\t",arr[m][l]);
+    }
+    printf("\n");
+}
+
+}
+
+
